a146ecd92011fee2eded922f11cb02f0f5476963
Luke Youngdong Ji <zephyrosjyd@gmail.com>
2020-05-31 16:15:24 +0900

    Add helloWorld query

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-console.log('Hey 👋');
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import depthLimit from 'graphql-depth-limit';
+import { createServer } from "http";
+import compression from 'compression';
+import cors from 'cors';
+import schema from './schema';
+
+const app = express();
+const server = new ApolloServer({
+  schema,
+  validationRules: [depthLimit(7)],
+});
+
+app.use('*', cors());
+app.use(compression());
+server.applyMiddleware({ app, path: '/graphql' });
+
+const httpServer = createServer(app);
+
+const port: number = 3000;
+httpServer.listen(
+  { port },
+  (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:${port}/graphql`)
+);
